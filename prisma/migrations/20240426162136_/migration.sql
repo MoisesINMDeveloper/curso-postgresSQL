5ffdad1f83d62b8728bd20d71c803b50b1e52f13
Moises Navarro <MoisesINMDeveloper@hotmail.com>
2024-04-26 12:21:57 -0400

    refactor

--- prisma/migrations/20240426162136_/migration.sql
+++ prisma/migrations/20240426162136_/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "user" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
 
     CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
